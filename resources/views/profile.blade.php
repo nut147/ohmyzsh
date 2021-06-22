9635a1597b51b50210a6e002aba3472bf8749007
Nuttawud Boonma <w.nut12345@gmail.com>
2021-06-22 13:26:10 +0700

    Submit Lab 1

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+        <title>profile</title>
+        </head> 
+            <body>
+                    <h1>Profile</h1>
+                    <h2>Basic Information</h2>
+                        <ul> 
+                                <li>Name : Nuttawud Boonma</li>
+                                <li>Gender: Female</li>
+                                <li>Age: 20</li>
+                                </ul>
+                        <h2>Autobiography</h2>
+                        <p>I was born on a warm, sunny day in Dec 12 in Chiang Rai, Thailand. I still live in Chiang Rai,Thailand,p>I live with my mom, dad sister.</p> 
+                        <p>and I go to school at TWK School. and naw t stratding in CAMT CMU </p> 
+                        
+
+                        <h2> My best Friend</h2>
+                        <ul> 
+                                <li>1.Poom</li>
+                                <li>2.Komgpop</li>
+                                <li>3.nut</li>
+                                </ul>
+
+            </body>
+</html>
